47da81c7f54e6705dacab35b97e9c13e0bfa5cb3
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-10-30 10:13:59 -0500

    remove links

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://www.txdot.gov/etc/designs/txdot/_jcr_content/global/header/logo.img.png/1496950554064.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World</p>
+        
+       
         <button>
           <a href="https://teams.microsoft.com/l/file/D79EE498-BB15-49C6-A200-F9911E24579A?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%202%20-%20Inspections%2F2.0%20Inspection%20Required%20Documentation%20and%20Notification(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
             target="blank">Chapter 2: Inspections</a>
@@ -19,14 +17,7 @@ function App() {
         <a href="https://www.txdot.gov"
             target="blank">TXDOT Home Page</a>
         </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
