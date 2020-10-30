201bdecc547a407a7c8dadd6512799d45d4b303c
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-10-30 09:49:02 -0500

    test deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ function App() {
         </p>
         <p>Hello World</p>
         <button>
-          Push Me
+          <a href="https://teams.microsoft.com/l/file/D79EE498-BB15-49C6-A200-F9911E24579A?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%202%20-%20Inspections%2F2.0%20Inspection%20Required%20Documentation%20and%20Notification(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+            target="blank">Chapter 2: Inspections</a>
+  
+        </button>
+        <button>
+        <a href="https://www.txdot.gov"
+            target="blank">TXDOT Home Page</a>
         </button>
         <a
           className="App-link"
