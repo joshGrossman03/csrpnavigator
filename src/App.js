53a12c49e578caa08515973744e8a5203361ac80
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-11-02 12:55:16 -0600

    new with auth

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Chapter1 from '../src/components/chapter1';
 import Chapter2 from '../src/components/chapter2';
 import Chapter3 from '../src/components/chapter3';
 import Chapter4 from '../src/components/chapter4';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import {AmplifySignOut} from '@aws-amplify/ui-react';
 
 
 function App() {
   return (
     <div className="App">
-
+<AmplifySignOut button-text="Custom Text"></AmplifySignOut>
 
       <div className="container-md">
 
         <div className="row" >
+        
           <div className="col-sm">
             <h4 style={{ color: "#1e265b", }}>Standard Working Instructions (SWI)</h4>
             <Chapter1></Chapter1>
@@ -36,4 +39,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
