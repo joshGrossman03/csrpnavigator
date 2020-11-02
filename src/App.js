9eb5360d1aa8756712c60c5bf2351f904fd0c5c2
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-11-02 03:03:48 -0600

    chapte1

--- src/App.js
+++ src/App.js
@@ -1,89 +1,28 @@
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
+
+
+import Chapter1 from '../src/components/chapter1';
+import Chapter2 from '../src/components/Chapter2';
 
 function App() {
   return (
     <div className="App">
 
-      <ul class="nav justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link active" href="https://www.google.com">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.google.com">SWI</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.google.com">Policies</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.google.com">Tools</a>
-        </li>
-      </ul>
+
       <div className="container-md">
-        <div className="row">
 
-          <div className="col-sm" style={{ backgroundColor: "#88829c" }}>
-            <h2 style={{ color: "#1e265b" }}>Commercial Signs Regulatory Program Navigator</h2>
-          </div>
-
-        </div>
         <div className="row" >
-          <div className="col-sm" style={{ backgroundColor: "#88829c", marginLeft: "0px" }}>
+          <div className="col-sm">
             <h4 style={{ color: "#1e265b", }}>Standard Working Instructions (SWI)</h4>
-            <Accordion>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    Chapter 1 - Program Administration
-      </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-
-                    <ListGroup>
-                      <ListGroup.Item><a href="https://teams.microsoft.com/l/file/AFF3B28A-2F48-4C1F-A12B-1C317643E28E?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.1%20Processing%20Of%20Paper%20Lockbox%20Receipts(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
-                        target="_blank"
-                        rel="noreferrer">1.1 Processing of Paper Lockbox Reciepts</a></ListGroup.Item>
-                      <ListGroup.Item><a href="https://teams.microsoft.com/l/file/574EFBA4-1EF0-437B-9ACB-78A80D35C121?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.2%20Processing%20Of%20Electronic%20Comptroller%20Receipts(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
-                        target="_blank"
-                        rel="noreferrer">1.2 Processing of Electronic Comptroller Reciepts</a></ListGroup.Item>
-                      <ListGroup.Item><a href="https://teams.microsoft.com/l/file/B22026F7-61F0-48D1-8980-EC025F3829EB?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.3%20Processing%20Of%20Checks%20Received%20For%20Commercial%20Signs(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
-                        target="_blank"
-                        rel="noreferrer">1.3 Processing of Checks Received for Commercial Signs</a></ListGroup.Item>
-                      <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/D7BC1756-B10D-4A16-BDBE-E3402EC4FDCF?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.4%20Updating%20Sign%20Owner%20Contact%20Information%20in%20EPS.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
-                      target="_blank"
-                      rel="noreferrer">1.4 Updating Sign Owner Contact Information in EPS</a> </ListGroup.Item>
-                      <ListGroup.Item><a href="https://teams.microsoft.com/l/file/E6789C0C-0943-4513-B0EE-5FA8E8C23A3C?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.5%20Permit%20Application%20Intake%20Procedures%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
-                      >1.5 Permit Application Intake Procedures</a> </ListGroup.Item>
-                    </ListGroup>
-
-
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    Chapter 2 - Inspections
-      </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-
-
+            <Chapter1></Chapter1>
+            <Chapter2></Chapter2>
           </div>
-          <div className="col-sm" style={{ backgroundColor: "#88829c" }}>
+          <div className="col-sm">
             <h4 style={{ color: "#1e265b", textAlign: "left" }}>Official Policies and Guidance</h4>
           </div>
-          <div className="col-sm" style={{ backgroundColor: "#88829c" }}>
+          <div className="col-sm">
             <h4 style={{ color: "#1e265b", textAlign: "left" }}>Useful Tools</h4>
           </div>
         </div>
---
+++ src/components/chapter1.js
@@ -0,0 +1,56 @@
+import Accordion from 'react-bootstrap/Accordion';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function Chapter1() {
+    return (
+        <Accordion>
+            <Card>
+                <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Chapter 1 - Program Administration
+      </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <ListGroup>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/AFF3B28A-2F48-4C1F-A12B-1C317643E28E?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.1%20Processing%20Of%20Paper%20Lockbox%20Receipts(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.1 Processing of Paper Lockbox Reciepts</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/574EFBA4-1EF0-437B-9ACB-78A80D35C121?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.2%20Processing%20Of%20Electronic%20Comptroller%20Receipts(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.2 Processing of Electronic Comptroller Reciepts</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/B22026F7-61F0-48D1-8980-EC025F3829EB?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.3%20Processing%20Of%20Checks%20Received%20For%20Commercial%20Signs(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.3 Processing of Checks Received for Commercial Signs</a></ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/D7BC1756-B10D-4A16-BDBE-E3402EC4FDCF?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.4%20Updating%20Sign%20Owner%20Contact%20Information%20in%20EPS.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.4 Updating Sign Owner Contact Information in EPS</a> </ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/E6789C0C-0943-4513-B0EE-5FA8E8C23A3C?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.5%20Permit%20Application%20Intake%20Procedures%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.5 Permit Application Intake Procedures</a> </ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/D5EFA49B-56EE-4311-BC40-1C7A98BA1E2F?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.6%20Impacted%20Commercial%20Sign%20Permit%20Application%20Intake%20Procedures(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.6 Impacted Commercial Sign Application Intake Procedures</a> </ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/521F33D7-72C9-4767-BD18-FF046050C7DA?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.7%20Processing%20License%20Applications.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.7 Procesing License Applications</a> </ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/17220077-21A0-4DE4-9CC3-AC9B6935C0C1?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.8%20License%20Suspensions%20and%20Revocations.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.8 License Suspensions and Revocations</a> </ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/095344B6-78BC-4A0B-AEF4-BDCEEF5F69C5?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%201%20-%20Program%20Administration%2F1.9%20CSRP%20Path%20for%20New%20Projects%20(SWI)%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">1.9 License Suspensions and Revocations</a> </ListGroup.Item>
+
+                        </ListGroup>
+
+
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    )
+}
+
+export default Chapter1;
