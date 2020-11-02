7faef2d24aea7c308c37effe4531675a5c1b252d
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-11-02 11:31:33 -0600

    chapter 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Chapter1 from '../src/components/chapter1';
 import Chapter2 from '../src/components/chapter2';
 import Chapter3 from '../src/components/chapter3';
+import Chapter4 from '../src/components/chapter4';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Chapter1></Chapter1>
             <Chapter2></Chapter2>
             <Chapter3></Chapter3>
+            <Chapter4></Chapter4>
           </div>
           <div className="col-sm">
             <h4 style={{ color: "#1e265b", textAlign: "left" }}>Official Policies and Guidance</h4>
---
+++ src/components/chapter4.js
@@ -0,0 +1,65 @@
+import Accordion from 'react-bootstrap/Accordion';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function Chapter4() {
+    return (
+        <Accordion>
+            <Card>
+                <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Chapter 2 - Inspections
+</Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+
+                        <ListGroup>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/9BBF5D0B-E493-4A89-944B-42029D713562?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FCIA%20Checklist%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">CIA Checklist</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/49EAA556-FF4E-4DD1-BB29-DE8EC60ED542?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FConformity%20and%20Nonconformity.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Conformity and Non-Conformity</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://teams.microsoft.com/l/file/C67CF4A9-204A-4930-8333-6758BE4A3DA4?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FCreating%20a%20TEA%20template%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Creating a TEA Template</a></ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/DEA9EBD9-CEB1-4062-A3E1-2F0FFE40DD1A?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FCreating%20a%20Trimble%20Offset%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Creating a Trimble Offset</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/083C1EF5-D91E-4823-839C-A179BF82567F?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FCreating%20Trimble%20Waypoints%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Creating Trimble Way Points</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/09EF1275-EB4C-415D-A5E8-FC820A3A12C5?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FExplorer%20app%20for%20Mobile%20Devices%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Explorer App for Mobile Devices</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/B69B6BF1-8E6F-4507-A325-D2D74094CF69?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FInstall%20a%20KMZ%20to%20Arcmap%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Install KMZ to ArcMap</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/225DE149-FC1D-44CE-B4D2-EEF210EDA538?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FProcessing%20the%20Trimble%20Data%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Processing Trimble GPS Data</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/14BB6B7F-E0D8-4685-AD2B-7B4B07343707?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FTrimble%20Data%20to%20Arc%20Map%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Importing Trimble GPS Data to ArcMap</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/9FAAC020-6B97-41C7-88C1-F39E354B12D3?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FTrimble%20Homescreen%20Shortcut%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">Trimble Homescreen Shortcut</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/916D33B6-7F7C-4B83-AC46-689DA6721DDE?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FTruPulse%20360%20Calibration%20Guide%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">TruPulse 360° Calibration Guide</a> </ListGroup.Item>
+                            <ListGroup.Item> <a href="https://teams.microsoft.com/l/file/83220760-1F09-4EA9-98B6-8EE904A85C22?tenantId=39dba476-5c09-4c63-91da-ce7a3ab5224d&fileType=pdf&objectUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram%2FShared%20Documents%2FGeneral%2FStandard%20Working%20Instructions%2FChapter%204%20-%20Reference%20Guides%2FTrupulse%20360%20Measurment%20Guide%20%20.pdf&baseUrl=https%3A%2F%2Ftxdot.sharepoint.com%2Fsites%2FCommercialSignsRegulatoryProgram&serviceName=teams&threadId=19:a9dbbb5be37e4ebebb0528d654bf482f@thread.skype&groupId=8a8a9d6f-713a-406d-8c3c-2fc8100a772f"
+                                target="_blank"
+                                rel="noreferrer">TruPulse 360° Measurement Guide</a> </ListGroup.Item>
+                        </ListGroup>
+
+
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    )
+}
+
+export default Chapter4;
